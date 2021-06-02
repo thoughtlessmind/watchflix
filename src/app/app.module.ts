31797d67c336874de3e88946f85abfd66abcbc17
Rajiv <1998irajeev@gmail.com>
2021-06-02 20:05:39 +0530

    integrated husky with prettier and eslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 const a = 'asdfasd';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,3 +13,7 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+// "lint-staged": {
+//   "*.js": "eslint --cache --fix"
+// }
